0e61517ad346051bb5359e053e5d0bdeb2552484
NoeliaSouza <noesouza@outlook.es>
2024-02-15 14:58:52 -0300

    Marca Crud

---
+++ Ecommerce.AccesoDatos/Repositorio/MarcaRepositorio.cs
@@ -0,0 +1,31 @@
+﻿using Ecommerce.AccesoDatos.Data;
+using Ecommerce.AccesoDatos.Repositorio.IRepositorio;
+using Ecommerce.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.AccesoDatos.Repositorio
+{
+    public class MarcaRepositorio : Repositorio<Marca>, IMarcaRepositorio
+    {
+        private readonly ApplicationDbContext _db;
+        public MarcaRepositorio(ApplicationDbContext db) : base(db) { // Le pasamos el db a la clase padre 
+            _db = db;
+        }
+
+        public void Actualizar(Marca marca)
+        {
+            var marcaDB = _db.Categorias.FirstOrDefault(b => b.Id == marca.Id);
+            if (marcaDB != null)
+            {
+                marcaDB.Nombre = marca.Nombre;
+                marcaDB.Descripcion = marca.Descripcion;
+                marcaDB.Estado = marca.Estado;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Ecommerce/Areas/Admin/Controllers/MarcaController.cs
@@ -0,0 +1,120 @@
+﻿using Ecommerce.AccesoDatos.Repositorio.IRepositorio;
+using Ecommerce.Modelos;
+using Ecommerce.Utilidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Ecommerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MarcaController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+
+        public MarcaController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            Marca marca = new Marca();
+
+            if (id == null)
+            {   //Creamos
+                marca.Estado = true;
+                return View(marca);
+            }
+            //Actualizamos
+            marca = await _unidadTrabajo.Marca.Obtener(id.GetValueOrDefault());
+            if (marca == null)
+            {
+                return NotFound();
+            }
+            return View(marca);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Marca marca)
+        {
+            if (ModelState.IsValid)
+            {
+                if (marca.Id == 0)
+                {
+                    await _unidadTrabajo.Marca.Agregar(marca);
+                    TempData[DS.Exitosa] = "Marca creada exitosamente";
+                }
+                else
+                {
+                    _unidadTrabajo.Marca.Actualizar(marca);
+                    TempData[DS.Exitosa] = "Marca actualizada exitosamente";
+                }
+                await _unidadTrabajo.Guardar();
+                return RedirectToAction(nameof(Index));
+            }
+            TempData[DS.Error] = "No se pudo realizar la operación";
+            return View(marca);
+        }
+
+
+
+
+        #region API
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var marca = await _unidadTrabajo.Marca.Obtener(id);
+            if (marca == null)
+            {
+                return Json(new { success = false, message = "Error al eliminar Marca" });
+            }
+            _unidadTrabajo.Marca.Remover(marca);
+            await _unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "Marca eliminada exitosamente" });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = await _unidadTrabajo.Marca.ObtenerTodos();
+            return Json(new { data = todos });
+        }
+
+        [ActionName("ValidarNombre")]
+        public async Task<IActionResult> ValidarNombre(string nombre, int id = 0)
+        {
+            bool valor = false;
+            var lista = await _unidadTrabajo.Marca.ObtenerTodos();
+
+            if (!nombre.IsNullOrEmpty())
+            {
+                if (id == 0)
+                {
+                    valor = lista.Any(b => b.Nombre.ToLower().Trim() == nombre.ToLower().Trim());
+                }
+                else
+                {
+                    valor = lista.Any(b => b.Nombre.ToLower().Trim() == nombre.ToLower().Trim()
+                    && b.Id != id);
+
+                }
+                if (valor)
+                {
+                    return Json(new { data = true });
+                }
+            }
+                
+            return Json(new { data = false });
+        }
+
+
+        #endregion
+
+    }
+}
