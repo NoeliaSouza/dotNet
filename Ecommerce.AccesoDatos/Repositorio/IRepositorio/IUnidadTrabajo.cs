32b88c105f85a1035a9201f247dc45719d18b647
NoeliaSouza <noesouza@outlook.es>
2024-02-16 16:47:12 -0300

    Producto CRUD

--- Ecommerce.AccesoDatos/Repositorio/IRepositorio/IUnidadTrabajo.cs
+++ Ecommerce.AccesoDatos/Repositorio/IRepositorio/IUnidadTrabajo.cs
@@ -11,6 +11,7 @@ namespace Ecommerce.AccesoDatos.Repositorio.IRepositorio
         IBodegaRepositorio Bodega { get;  }
         ICategoriaRepositorio Categoria { get; }    
         IMarcaRepositorio Marca { get; }    
+        IProductoRepositorio Producto { get; }    
         Task Guardar();
     }
 }
---
+++ Ecommerce.AccesoDatos/Repositorio/ProductoRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using Ecommerce.AccesoDatos.Data;
+using Ecommerce.AccesoDatos.Repositorio.IRepositorio;
+using Ecommerce.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.AccesoDatos.Repositorio
+{
+    public class ProductoRepositorio : Repositorio<Producto>, IProductoRepositorio
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductoRepositorio(ApplicationDbContext db) : base(db) { // Le pasamos el db a la clase padre 
+            _db = db;
+        }
+
+        public void Actualizar(Producto producto)
+        {
+            var productoDB = _db.Productos.FirstOrDefault(b => b.Id == producto.Id);
+            if (productoDB != null)
+            {   
+                if (producto.ImagenUrl != null)
+                {
+                    productoDB.ImagenUrl=producto.ImagenUrl;
+                }
+
+                productoDB.Codigo=producto.Codigo;
+                productoDB.Descripcion=producto.Descripcion;
+                productoDB.Precio=producto.Precio;
+                productoDB.Costo=producto.Costo;
+                productoDB.CategoriaId=producto.CategoriaId;
+                productoDB.MarcaId=producto.MarcaId;
+                productoDB.PadreId=producto.PadreId;
+                productoDB.Estado=producto.Estado;
+               
+
+                _db.SaveChanges();
+            }
+        }
+    }
+}
