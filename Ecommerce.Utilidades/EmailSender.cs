c7ce26011433716ceb38ff9a11320510b4cabf2f
NoeliaSouza <noesouza@outlook.es>
2024-03-01 16:00:24 -0300

    Envio de email

--- Ecommerce.Utilidades/EmailSender.cs
+++ Ecommerce.Utilidades/EmailSender.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +12,19 @@ namespace Ecommerce.Utilidades
 {
     public class EmailSender : IEmailSender
     {
+
+        public string SendGrid { get; set; }
+        public EmailSender(IConfiguration _config)
+        {
+            SendGrid = _config.GetValue<string>("Key");
+        }
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            throw new NotImplementedException();
+            var cliente = new SendGridClient(SendGrid);
+            var from = new EmailAddress("email@hotmail.com");
+            var to = new EmailAddress(email);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+            return cliente.SendEmailAsync(msg);
         }
     }
 }
